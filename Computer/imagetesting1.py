3dfaac600b0d5cf21ba45ae632c73bfef9e31190
Megan Moore <megan.moore@uq.net.au>
2014-08-26 12:36:45 +1000

    Initial image processing

---
+++ Computer/imagetesting1.py
@@ -0,0 +1,12 @@
+#Commented parts open image using Pygame library
+
+
+from PIL import Image
+#import pygame
+
+#im= Image.open('C:\\Users\\Megan\\Documents\\ENGG2800Python\\TestPattern 32x32 p1.pbm')
+im = Image.open("C:\\Users\\Megan\\Documents\\ENGG2800Python\\TestPattern 32x32 p1.pbm")
+im.show()
+#im1=pygame.image.load('C:\\Users\\Megan\\Documents\\ENGG2800Python\\TestPattern 32x32 p1.pbm')
+#print im1
+print im
---
+++ Computer/imagetesting2.py
@@ -0,0 +1,7 @@
+#from PIL import Image
+#fp = open("C:\\Users\\Megan\\Documents\\ENGG2800Python\\p1-pbm-ascii\\TestPattern 32x32 p1.pbm", "rb")
+#im = Image.open(fp)
+#print fp
+#im.show()
+#fp.readlines()
+

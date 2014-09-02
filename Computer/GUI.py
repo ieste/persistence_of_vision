d5fab73bb95667d7f2a5fea0ba5e7381e1ccca92
Megan Moore <megan.moore@uq.net.au>
2014-09-02 13:10:26 +1000

    Displays text and binary format images

--- Computer/GUI.py
+++ Computer/GUI.py
@@ -1,7 +1,8 @@
 from Tkinter import *
-
-#REFERENCE ALPHABET
-ABET = {'A' : [30,33,33,63,33,33,33]}
+import tkFileDialog
+from PIL import Image, ImageTk, ImageDraw, ImageFont
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class POVApp(object):
     """Top level POV Display application"""
@@ -32,19 +33,74 @@ class POVApp(object):
         self.text_buttons.grid(row=1, column=1, pady=10, sticky=W)
 
         self.upload_text = Button(self.text_buttons, text='Upload',
-                                  command=self.upload)
+                                  command=self.upload_text)
         self.upload_text.pack(fill=X, ipadx=30, side=LEFT)
         
-        self.preview_text = Button(self.text_buttons, text='Preview') # command)
+        self.preview_text = Button(self.text_buttons, text='Preview',
+                                  command=self.preview_text)
         self.preview_text.pack(fill=X, side=LEFT, ipadx=30, padx=17)
+        
+        #Image Frame
+        self.image = Frame(root)
+        self.image.pack(expand=True, pady=30, padx=30)
 
-    def upload(self):
-        #Only accepts 1 char 'A'
-        self.letter = ABET[self.text_entry.get()]
-        print self.text_entry.get()
-        for self.line in self.letter:
-            print "{:06b}".format(self.line)
-        #bin(i) converts i in A to binary number
+        self.instruct2 = Label(self.image, text='Image: ')
+        self.instruct2.grid(row=0)
+
+        self.find_image = Button(self.image, text='Find...', width=27,
+                                 command=self.OpenImage)
+        self.find_image.grid(row=0, column=1, sticky=W)
+
+        self.image_buttons = Frame(self.image)
+        self.image_buttons.grid(row=1, column=1, pady=10, sticky=W)
+
+        self.upload_image = Button(self.image_buttons, text='Upload',
+                                  command=self.upload_image)
+        self.upload_image.pack(fill=X, ipadx=30, side=LEFT)
+        
+        self.preview_image = Button(self.image_buttons, text='Preview',
+                                  command=self.preview_image)
+        self.preview_image.pack(fill=X, side=LEFT, ipadx=30, padx=17)
+
+    
+    def upload_text(self):
+        return NULL
+    
+    def preview_text(self):
+        self.displaytext = Toplevel()
+        
+        self.text = self.text_entry.get()
+        self.img = Image.new('RGB', (360, 32), "white")
+        self.d = ImageDraw.Draw(self.img)
+        self.text_width, self.text_height = self.d.textsize(self.text)
+        self.d.text((((180-((self.text_width)/2)), (16-(self.text_height)/2))),
+                    self.text, fill=("black"))
+        self.t = ImageTk.PhotoImage(self.img)
+        
+        self.canvas1 = Canvas(self.displaytext, width=360, height=32)
+        self.canvas1.pack(expand=YES, fill=BOTH)
+        self.canvas1.create_image((180,16),image=self.t)
+
+        #img.save("image.png")
+        #self.img.show()
+
+    def OpenImage(self):
+        self.imagefile = tkFileDialog.askopenfilename()
+        self.f=open(self.imagefile, "r")
+        self.im=Image.open(self.f)
+        #self.im.show()
+
+    def upload_image(self):
+        return NULL
+    
+    def preview_image(self):
+        self.displayimage = Toplevel()
+
+        self.i = ImageTk.PhotoImage(self.im)
+        
+        self.canvas2 = Canvas(self.displayimage, width=360, height=32)
+        self.canvas2.pack(expand=YES, fill=BOTH, padx=3, pady=3)
+        self.canvas2.create_image((180,16), image=self.i)
 
 root = Tk()
 app = POVApp(root)

84fbed3b24479301e2c1456a0d27de2cdbd6ddff
Megan Moore <megan.moore@uq.net.au>
2014-09-17 14:20:36 +1000

    Updated p1 parser, concatenates image data into single string. Needs to be split per image dimensions.

--- Computer/imagetesting2.py
+++ Computer/imagetesting2.py
@@ -4,40 +4,62 @@ from PIL import Image, ImageTk, ImageDraw, ImageFont
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-fp = open("C:\\Users\\Megan\\Documents\\ENGG2800\\engg2800-team33\\Computer\\p1-pbm-ascii\\TestPattern 32x32 p1.pbm", "rb")
+#this image works
+#fp = open("C:\\Users\\Megan\\Documents\\ENGG2800\\engg2800-team33\\Computer\\p1-pbm-ascii\\TestPattern 32x32 p1.pbm", "rU")
+
+#this does not
+fp = open("C:\\Users\\Megan\\Documents\\ENGG2800\\engg2800-team33\\Computer\\p1-pbm-ascii\\It works 145x32 p1.pbm", "rU")
 image = []
+imagestr = []
 #im = Image.open(fp)
+
+#read in first two lines of image(type and size)
 print fp
-#im.show()
 img_type = fp.readline()
 img_size = fp.readline()
-for line in fp:
-    line = line.strip()
-    image.append(list(line))
 
-#Matrix Size
-i=0
-j=0
-for row in image:
-    i=i+1
-for col in image[1]:
-    j=j+1
+#get image size
+print img_type
+image_size=[]
+for s in img_size.split():
+    image_size.append(int(s))
+size=(image_size[0], image_size[1])
 
+#print fp.readlines()
+
+#append matrix with image data
+for line in fp.readlines():
+    line=line.strip()
+    imagestr.append(line)
+imagestr=''.join((image))
+
+print imagestr[:(size[0])]
+image.append(imagestr[:size[0]])
+
+print image
+
+
+#append matrix with image data
+for line in fp.readlines():
+    image.append(line)
+    #sys.stdout.write(line)
+
+
+    
 fp.close()
-im = Image.new("RGB", (j, i), "white")
+im = Image.new("RGB", size, "white")
 draw = ImageDraw.Draw(im)
 #print image
 
-x=0
 y=0
 
 for row in image:
     x=0
     for col in row:
-        a = image[x][y]
-        if a == '1':
+        a=image[y][x]
+        if a == '0':
             draw.point((x,y), fill="black")
-        x=x+1
-    y=y+1
-im.show()
+        x+=1
+    y+=1
+#im.show()
 #fp.readlines()

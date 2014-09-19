9c57ce0aab9f55b1f25c3b74e107441955fe12ec
Isabella Stephens <stephens.isabella@gmail.com>
2014-09-20 02:00:10 +1000

    Updated get_info function to look for max_val, added Megan's P1 image parsing code.

--- Computer/ImageParser.py
+++ Computer/ImageParser.py
@@ -1,16 +1,17 @@
 import os.path
 import string
 import sys
-from PIL import Image
-import io
+from PIL import Image, ImageDraw
 
 
 def get_info(path_or_file):
 
-    #if path_or_file is
+    # if path_or_file is file, assign to image_file and seek to start of file
     if isinstance(path_or_file, file):
         image_file = path_or_file
         image_file.seek(0, 0)
+
+    # otherwise open the file
     else:
         # Check that file path points to a path
         if os.path.isfile(path_or_file):
@@ -25,15 +26,15 @@ def get_info(path_or_file):
         sys.stderr.write("Invalid file type.")
         return
 
-    # Return if there is no whitespace separating the dimensions
+    # Return if there is no whitespace separating the dimensions from the type
     if image_file.read(1) not in string.whitespace:
         sys.stderr.write("Whitespace missing between type and dimensions.")
         return
 
-    width = 0
-    height = 0
+    width, height, max_val = 0, 0, 0
     finished = 0
 
+    # Read in dimensions and max value, discarding comments
     while not finished:
         byte = image_file.read(1)
 
@@ -57,6 +58,14 @@ def get_info(path_or_file):
                 while byte not in string.whitespace:
                     height += byte
                     byte = image_file.read(1)
+                if file_type in ['P1', 'P4']:
+                    finished = 1
+            elif max_val is 0:
+                height = byte
+                byte = image_file.read(1)
+                while byte not in string.whitespace:
+                    height += byte
+                    byte = image_file.read(1)
                 finished = 1
 
         else:
@@ -64,8 +73,8 @@ def get_info(path_or_file):
             return
 
     # If width or height read in are not valid then return None
-    if not width.isdigit() or not height.isdigit():
-        sys.stderr.write("Invalid width or height.")
+    if not width.isdigit() or not height.isdigit() or not max_val.isdigit():
+        sys.stderr.write("Invalid width, height or max_val.")
         return
 
     # Discard any comments remaining before the image data
@@ -77,39 +86,31 @@ def get_info(path_or_file):
     if byte not in string.whitespace:
         return
 
-    return file_type, (int(width), int(height))
+    return file_type, (int(width), int(height)), max_val
 
 
-def parse_image(image_file):
+def P4_parser(image_file):
 
-    file_type, (width, height) = get_info(image_file)
+    # Get the image dimensions
+    (width, height) = get_info(image_file)[1]
     dimensions = (width, height)
 
-    if file_type in ['P1', 'P4']:
-        mode = '1'
-    elif file_type in ['P2', 'P5']:
-        mode = 'L'
-    else:
-        mode = 'RGB'
+    # Create an image to hold the image data
+    image = Image.new('L', dimensions, 0)
 
-    image = Image.new(mode, dimensions, 0)
-
-    #P4: need to pad width out to 8:
+    # Need to pad width out to 8 bits (rows are stored in full bytes).
     row_length = width/8
     if width % 8 is not 0:
         row_length = (width + 8 - width % 8)/8
 
+    # Read in all the image data.
     contents = image_file.read(row_length*height)
 
-    # Loop through each bit read in and convert it to a byte
-    # 0 -> 255, 1 -> 0
+    # Loop through each bit read in and convert it to a byte.
     data = ""
-    print width
-    print height
     for i in range(height):
         for j in range(width):
             position = i * row_length + j/8
-            #print position, 7 - j % 8
             if ord(contents[position]) & (1 << 7 - j % 8):
                 data += chr(0)
                 #sys.stdout.write('X')
@@ -118,20 +119,60 @@ def parse_image(image_file):
                 #sys.stdout.write('.')
         #sys.stdout.write('\n')
 
+    # Populate the image with the data
     image.putdata(data)
-
-
 
     return image
 
 
+def P1_parser(image_file):
+
+    # Read in image dimensions
+    (width, height) = get_info(image_file)[1]
+    dimensions = (width, height)
+
+    # Create an image
+    image = Image.new("RGB", dimensions, "white")
+
+    # Read in image data
+    image_str = []
+    for line in image_file.readlines():
+        line = line.strip()
+        image_str.append(line)
+
+    # Convert image_str into a string rather than a list of strings.
+    image_str = ''.join(image_str)
+
+    # Set black pixels
+    for x in range(width):
+        for y in range(height):
+            if image_str[y * width + x] is '1':
+                image.putpixel((x, y), 0)
+
+    return image
 
 
+def P2_parser(image_file):
 
-path = "C:\\Users\\Isabella\\Desktop\\TestPattern 32x32 p4.pbm"
-image = open(path, 'rb')
-look = parse_image
-look(image)
+    t, (width, height), max_val = get_info(image_file)
+    dimensions = (width, height)
+
+    image = Image.new("RGB", dimensions, "white")
+
+    return image
+
+def parse_image(image_file):
+
+    file_type = get_info(image_file)[0]
+
+    parser = globals().get(file_type + "_parser")
+
+    if not parser:
+        sys.stderr.write("Parser for " + file_type + " not found.")
+        return
+
+    return parser(image_file)
+
 
 
 

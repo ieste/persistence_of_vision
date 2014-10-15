9d573e503c1d4542abcc7141f069ddfb0c0d2c61
Isabella Stephens <stephens.isabella@gmail.com>
2014-10-16 01:35:15 +1000

    Improvements to GUIeditor.py

--- Computer/GUIeditor.py
+++ Computer/GUIeditor.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 import tkFileDialog
+import tkColorChooser
 from PIL import Image, ImageTk, ImageDraw, ImageFont, ImageColor
 from PIL import ImageFile
 import math
@@ -17,12 +18,12 @@ class InvalidFile(object):
 
 class POVApp(object):
     """Top level POV Display application"""
-    
+
     def __init__(self, root):
         #Main Window
         root.title('POV Wheel')
         self.w, self.h = 360, 32
-        
+
         #Menu Bar
         self.menu = Menu(root)
         self.file = Menu(self.menu, tearoff=0)
@@ -32,19 +33,54 @@ class POVApp(object):
         self.file.add_command(label="Upload", command = self.upload_image)
         self.file.add_command(label="Exit", command = root.destroy)
         self.menu.add_cascade(label="File", menu = self.file)
-        root.config(menu=self.menu, padx=5)
+        root.config(menu=self.menu)
 
         #Toolbar frame
         self.toolbar = Frame(root)
-        self.toolbar.grid(row=0, column=0)
+        self.toolbar.grid(row=0, column=0, padx=(6,3), pady=6)
+
+        #Set Colour - intialise colour as white
+        self.colour = StringVar()
+        self.colour = "#FFFFFF"
+
+        self.c_select = Label(self.toolbar, bg=self.colour, relief=RAISED)
+        self.c_select.pack(ipadx=12, pady=10)
+        self.c_select.bind("<Button-1>", self.c_select_click)
+
+        #Tool Buttons
+        self.draw = Button(self.toolbar, text = "Draw")
+        self.draw.pack(pady=2, fill=X)
+        self.line = Button(self.toolbar, text = "Line")
+        self.line.pack(pady=2, fill=X)
+        self.clear = Button(self.toolbar, text = "Clear")
+        self.clear.pack(pady=2, fill=X)
+        self.erase  = Button(self.toolbar, text = "Erase")
+        self.erase.pack(pady=2, fill=X)
+        self.square = Button(self.toolbar, text = "Square")
+        self.square.pack(pady=2, fill=X)
+
+        #Tools Mouse Bind for tool buttons
+        self.draw.bind("<Button-1>", self.draw_click)
+        self.clear.bind("<Button-1>", self.clear_click)
+        self.erase.bind("<Button-1>", self.erase_click)
+        self.square.bind("<Button-1>", self.square_click)
+        self.line.bind("<Button-1>", self.line_click)
 
         #Canvas
-        self.canvas = Canvas(root, bg="light grey", height=200, width=400)
-        self.canvas.grid(row=0, column=1)
-        
+        self.canvas = Canvas(root, bg="light grey", width=400, height=200, highlightthickness=0)
+        self.canvas.grid(row=0, column=1, sticky=N+W+E+S, padx=(3,6), pady=6)
+        root.columnconfigure(1, weight=1)
+
+        self.canvas.bind("<Configure>", self.on_canvas_resize)
+
         #Create image on canvas
         self.new()
-        
+
+        #Canvas Mouse Bind
+        self.canvas.bind("<Motion>", self.mouse_motion)
+        self.canvas.bind("<Button-1>", self.mouse_click)
+        self.canvas.bind("<ButtonRelease-1>", self.mouse_release)
+
         #Text Frame
         self.text = Frame(root)
         self.text.grid(row=1, column=1)
@@ -67,183 +103,159 @@ class POVApp(object):
         self.preview_button = Button(self.select_size, text = 'Preview', command = self.preview)
         self.preview_button.pack(side=LEFT, padx = 30, ipadx = 30)
 
-        #Set Colour - intialise colour as black
-        self.colour = StringVar()
-        self.colour = "#000000"
-        
-        #Colours Frame - in toolbar frame
-        self.colours = Frame(self.toolbar)
-        self.colours.grid(row=0, column=0, padx=10)
 
-        #Colour Labels - in colours frame
-        self.c_select = Label(self.colours, bg = self.colour, relief=RAISED)
-        self.c_select.pack(ipadx=12, pady=10)
-        self.c1 = Label(self.colours, bg = '#ffffff', relief=SUNKEN)
-        self.c1.pack(ipadx=12, pady=2)
-        self.c2 = Label(self.colours, bg = '#cccccc', relief=SUNKEN)
-        self.c2.pack(ipadx=12, pady=2)
-        self.c3 = Label(self.colours, bg = '#999999', relief=SUNKEN)
-        self.c3.pack(ipadx=12, pady=2)
-        self.c4 = Label(self.colours, bg = '#666666', relief=SUNKEN)
-        self.c4.pack(ipadx=12, pady=2)
-        self.c5 = Label(self.colours, bg = '#333333', relief=SUNKEN)
-        self.c5.pack(ipadx=12, pady=2)
-        self.c6 = Label(self.colours, bg = '#000000', relief=SUNKEN)
-        self.c6.pack(ipadx=12, pady=2)
-        
-        # Tools Frame
-        self.tools = Frame(self.toolbar)
-        self.tools.grid(row=0, column=1, padx=5)
-        
-        #Tool Buttons - in tools frame
-        self.draw = Button(self.tools, text = "Draw")
-        self.draw.pack(pady=2, fill=X)
-        self.line = Button(self.tools, text = "Line")
-        self.line.pack(pady=2, fill=X)
-        self.fill = Button(self.tools, text = "Fill")
-        self.fill.pack(pady=2, fill=X)
-        self.erase  = Button(self.tools, text = "Erase")
-        self.erase.pack(pady=2, fill=X)
-        self.square = Button(self.tools, text = "Square")
-        self.square.pack(pady=2, fill=X)
-        
-        #Canvas Mouse Bind
-        self.canvas.bind("<Motion>", self.mouse_motion)
-        self.canvas.bind("<Button-1>", self.mouse_click)
-        self.canvas.bind("<ButtonRelease-1>", self.mouse_release)
-
-        #Colour Mouse Bind for colour labels
-        self.c1.bind("<Button-1>", self.c1_click)
-        self.c2.bind("<Button-1>", self.c2_click)
-        self.c3.bind("<Button-1>", self.c3_click)
-        self.c4.bind("<Button-1>", self.c4_click)
-        self.c5.bind("<Button-1>", self.c5_click)
-        self.c6.bind("<Button-1>", self.c6_click)
-
-        #Tools Mouse Bind for tool buttons
-        self.draw.bind("<Button-1>", self.draw_click)
-        self.fill.bind("<Button-1>", self.fill_click)
-        self.erase.bind("<Button-1>", self.erase_click)
-        self.square.bind("<Button-1>", self.square_click)
-        self.line.bind("<Button-1>", self.line_click)
-
-    #The following functions select colours based on the colour label that
-    #is clicked
-    def c1_click(self, e):
-        self.colour = '#ffffff'
-        self.c_select.configure(bg='#ffffff')
-
-    def c2_click(self, e):
-        self.colour = '#cccccc'
-        self.c_select.configure(bg='#cccccc')
-
-    def c3_click(self, e):
-        self.colour = '#999999'
-        self.c_select.configure(bg='#999999')
-
-    def c4_click(self, e):
-        self.colour = '#666666'
-        self.c_select.configure(bg='#666666')
-
-    def c5_click(self, e):
-        self.colour = '#333333'
-        self.c_select.configure(bg='#333333')
-
-    def c6_click(self, e):
-        self.colour = '#000000'
-        self.c_select.configure(bg='#000000')
-
-    #Binds events to line when line button is pressed
-    def line_click(self, e):
-        self.canvas.bind("<Button-1>", self.line_start)
-        self.canvas.bind("<ButtonRelease-1>", self.line_end)
-        self.canvas.bind("<B1-Motion>", self.mouse_motion)
-
-    #Sets the intial coordinates for the line draw function
-    def line_start(self, e):
-        self.x, self.y = (e.x-((360-self.w)/2)-19, e.y-84)
-        
-    #Sets the end coordinates for the line draw function
-    def line_end(self, e):
-        x0,y0 = (self.x, self.y)
-        x1,y1 = (e.x-((360-self.w)/2)-19, e.y-84)
-        self.d = ImageDraw.Draw(self.img)
-        self.d.line([x0, y0, x1, y1], fill=self.colour, width=2)
+    #Returns image to blank, 360 pixel wide
+    def new(self):
+        self.w, self.h = 360, 32
+        self.img = Image.new('RGB', (self.w, self.h), "black")
         self.t = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(200, 100, image=self.t)
+        self.tid = self.canvas.create_image(200, 100, image=self.t)
 
-    #Probably don't need this function, will probably delete
-    def fill_click(self, e):
-        self.canvas.bind("<Button-1>", self.draw_fill)
+    #Center the image when the canvas is resized
+    def on_canvas_resize(self, e):
+        cw, ch = self.canvas.winfo_width(), self.canvas.winfo_height() # Canvas width & height
+        self.cxc, self.cyc = cw/2, ch/2 # Canvas x center & y center
+        self.canvas.coords(self.tid, self.cxc, self.cyc)
 
-    #To be deleted
-    def draw_fill(self, e):
-        return 0
+    #Translate coordinates from the canvas reference to the image reference.
+    def translate_coords(self, x, y):
+        return x-self.cxc+self.w/2, y-self.cyc+self.h/2
 
-    #Binds events to erase when erase button is pressed
-    def erase_click(self, e):
-        self.canvas.bind("<B1-Motion>", self.draw_erase)
-        self.canvas.bind("<ButtonRelease-1>", self.mouse_motion)
-        
-    #Erase function - draws a white circle of 10 pixel diameter
-    def draw_erase(self, e):
-        x0,y0 = (e.x-((360-self.w)/2)-19, e.y-84)
-        self.d = ImageDraw.Draw(self.img)
-        self.d.ellipse([x0 ,y0, x0+10, y0+10], fill = 'white', outline='white')
-        self.t = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(200, 100, image=self.t)
+    #Show color chooser
+    def c_select_click(self, e):
+        crgb, chex = tkColorChooser.askcolor(self.colour)
+        if chex is not None:
+            self.colour = chex
+            self.c_select.configure(bg=chex)
 
-    #To be implemented if time permits    
-    def square_click(self, e):
-        return 0
-
-    #Binds events to draw when draw button is pressed    
-    def draw_click(self, e):
-        self.canvas.bind("<Button-1>", self.start_draw)
-        self.canvas.bind("<B1-Motion>", self.draw_draw)
-        self.canvas.bind("<ButtonRelease-1>", self.mouse_motion)
-
-    #Initialises draw coordinates, and draws single pixel at point
-    def start_draw(self, e):
-        self.x0,self.y0 = (e.x-((360-self.w)/2)-19, e.y-84)
-        self.d = ImageDraw.Draw(self.img)
-        self.d.point((self.x0, self.y0), fill=self.colour)
-        self.t = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(200, 100, image=self.t)
-
-    #Establishes start and end coordinates for draw when mouse in motion
-    def draw_draw(self, e):
-        self.x1,self.y1 = (e.x-((360-self.w)/2)-19, e.y-84)
-        self.connect_draw(self.x0, self.y0, self.x1, self.y1)
-        self.x0, self.y0 = self.x1, self.y1
-        self.d = ImageDraw.Draw(self.img)
-        self.t = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(200, 100, image=self.t)
-
-    #Connects the start and end coordinates to create fluid line
-    #Updates with new coordinates
-    def connect_draw(self, x0, y0, x1, y1):
-        self.d = ImageDraw.Draw(self.img)
-        self.d.line([x0, y0, x1, y1], fill=self.colour)
-        self.t = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(200, 100, image=self.t)
-        self.x0, self.y0 = x1, y1
-
-    #Default state for mouse motion
-    def mouse_motion(self, e):
-        print (e.x-((360-self.w)/2)-19, e.y-84)
+    ## Default
 
     #Default state for mouse click
     def mouse_click(self, e):
         return 0
 
+    #Default state for mouse motion
+    def mouse_motion(self, e):
+        print self.translate_coords(e.x, e.y)
+
     #Default state for mouse release
     def mouse_release(self, e):
         return 0
 
+    ## Draw
+
+    #Binds events to draw when draw button is pressed    
+    def draw_click(self, e):
+        self.canvas.bind("<Button-1>", self.draw_start)
+        self.canvas.bind("<B1-Motion>", self.draw_motion)
+        self.canvas.bind("<ButtonRelease-1>", self.mouse_release)
+
+    #Initialises draw coordinates, and draws single pixel at point
+    def draw_start(self, e):
+        self.x0, self.y0 = self.translate_coords(e.x, e.y)
+        self.d = ImageDraw.Draw(self.img)
+        self.d.point((self.x0, self.y0), fill=self.colour)
+        self.t = ImageTk.PhotoImage(self.img)
+        self.canvas.itemconfig(self.tid, image=self.t)
+
+    #Establishes start and end coordinates for draw when mouse in motion
+    def draw_motion(self, e):
+        self.x1, self.y1 = self.translate_coords(e.x, e.y)
+        self.d = ImageDraw.Draw(self.img)
+        self.d.line([self.x0, self.y0, self.x1, self.y1], fill=self.colour)
+        self.t = ImageTk.PhotoImage(self.img)
+        self.canvas.itemconfig(self.tid, image=self.t)
+        self.x0, self.y0 = self.x1, self.y1
+
+    ## Line
+
+    #Binds events to line when line button is pressed
+    def line_click(self, e):
+        self.canvas.bind("<Button-1>", self.line_start)
+        self.canvas.bind("<B1-Motion>", self.line_motion)
+        self.canvas.bind("<ButtonRelease-1>", self.line_end)
+
+    #Sets the intial coordinates for the line draw function
+    def line_start(self, e):
+        self.x, self.y = self.translate_coords(e.x, e.y)
+
+    def line_motion(self, e):
+        x0,y0 = (self.x, self.y)
+        x1,y1 = self.translate_coords(e.x, e.y)
+        temp_img = self.img.copy()
+        self.d = ImageDraw.Draw(temp_img)
+        self.d.line([x0, y0, x1, y1], fill=self.colour, width=2)
+        self.t = ImageTk.PhotoImage(temp_img)
+        self.canvas.itemconfig(self.tid, image=self.t)
+
+    #Sets the end coordinates for the line draw function
+    def line_end(self, e):
+        x0,y0 = (self.x, self.y)
+        x1,y1 = self.translate_coords(e.x, e.y)
+        self.d = ImageDraw.Draw(self.img)
+        self.d.line([x0, y0, x1, y1], fill=self.colour, width=2)
+        self.t = ImageTk.PhotoImage(self.img)
+        self.canvas.itemconfig(self.tid, image=self.t)
+
+    ## Clear
+
+    #Clear the image
+    def clear_click(self, e):
+        self.img = Image.new('RGB', (self.w, self.h), "black")
+        self.t = ImageTk.PhotoImage(self.img)
+        self.tid = self.canvas.create_image(self.cxc, self.cyc, image=self.t)
+
+    ## Erase
+
+    #Binds events to erase when erase button is pressed
+    def erase_click(self, e):
+        self.canvas.bind("<Button-1>", self.erase_start)
+        self.canvas.bind("<B1-Motion>", self.erase_motion)
+        self.canvas.bind("<ButtonRelease-1>", self.mouse_release)
+
+    #Initialises draw coordinates, and draws single pixel at point
+    def erase_start(self, e):
+        self.x0,self.y0 = self.translate_coords(e.x, e.y)
+        self.d = ImageDraw.Draw(self.img)
+        self.d.ellipse([self.x0-5, self.y0-5, self.x0+5, self.y0+5], fill='black', outline='black')
+        self.t = ImageTk.PhotoImage(self.img)
+        self.canvas.itemconfig(self.tid, image=self.t)
+
+    #Establishes start and end coordinates for draw when mouse in motion
+    def erase_motion(self, e):
+        self.x1,self.y1 = self.translate_coords(e.x, e.y)
+        self.d = ImageDraw.Draw(self.img)
+        self.d.line([self.x0, self.y0, self.x1, self.y1], fill='black', width=10)
+        self.d.ellipse([self.x1-5, self.y1-5, self.x1+5, self.y1+5], fill='black', outline='black')
+        self.t = ImageTk.PhotoImage(self.img)
+        self.canvas.itemconfig(self.tid, image=self.t)
+        self.x0, self.y0 = self.x1, self.y1
+
+    ## Square
+
+    #To be implemented if time permits
+    def square_click(self, e):
+        return 0
+
+    ## Text
+
+    #Draws text from entry widget onto image centred
+    def preview_text(self, e):
+        self.text = self.text_entry.get()
+        self.d = ImageDraw.Draw(self.img)
+        self.text_width, self.text_height = self.d.textsize(self.text)
+        
+        self.d.text((((self.w/2-((self.text_width)/2)), (self.h/2.0-(self.text_height)/2))),
+                    self.text, fill=self.colour)
+        self.t = ImageTk.PhotoImage(self.img)
+        self.canvas.itemconfig(self.tid, image=self.t)
+
+    ## Resize
+
     #Resizes the image width to number in entry widget
     #Will not resize larger than 360 or smaller than 1 pixel
     def enter_width(self, e):
+        oldw = self.w
         self.w = self.width_entry.get()
         if self.w == '':
             return 0
@@ -252,20 +264,13 @@ class POVApp(object):
             self.w = 360
         if self.w < 1:
             self.w = 1
-        self.img = Image.new('RGB', (self.w, self.h), "white")
+        oldimg = self.img
+        self.img = Image.new('RGB', (self.w, self.h), "black")
+        self.img.paste(oldimg, ((self.w-oldw)/2, 0))
         self.t = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(200, 100, image=self.t)
+        self.canvas.itemconfig(self.tid, image=self.t)
 
-    #Draws text from entry widget onto image centred
-    def preview_text(self, e):
-        self.text = self.text_entry.get()
-        self.d = ImageDraw.Draw(self.img)
-        self.text_width, self.text_height = self.d.textsize(self.text)
-        
-        self.d.text((((self.w/2-((self.text_width)/2)), (self.h/2-(self.text_height)/2))),
-                    self.text, fill=self.colour)
-        self.t = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(200, 100, image=self.t)
+    ## Preview
 
     #Opens new window with image displayed as would appear on the POV display
     def preview(self):
@@ -333,6 +338,8 @@ class POVApp(object):
         self.piphoto = ImageTk.PhotoImage(self.preview_image)
         self.preview_canvas.create_image(48,48, image=self.piphoto)
 
+    ## Menu
+
     #Opens image through Image Parser
     def OpenImage(self):
         self.imagefile = tkFileDialog.askopenfilename()
@@ -345,7 +352,7 @@ class POVApp(object):
             self.h = self.size[1]
             self.t = ImageTk.PhotoImage(self.img)
             self.canvas.delete(ALL)
-            self.canvas.create_image(200, 100, image=self.t)
+            self.canvas.itemconfig(self.tid, image=self.t)
         except InvalidFile as e:
             tkMessageBox.showwarning(title="Invalid File",
                                      message=e)
@@ -388,13 +395,6 @@ class POVApp(object):
             print "USB Device not found."
             return
         avr.write_pages(ImageParser.image_to_data(self.img))
-
-    #Returns image to blank, 360 pixel wide
-    def new(self):
-        self.w, self.h = 360, 32
-        self.img = Image.new('RGB', (self.w, self.h), "white")
-        self.t = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(200, 100, image=self.t)
 
 root = Tk()
 app = POVApp(root)
